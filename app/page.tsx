ef23ed1a54c3e55fd060a47672efccce7a605ba9
Gabriel Caldeira <gabrielcaldeira270@gmail.com>
2024-07-14 18:51:42 -0300

    buttons with shared state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 'use client';
+import { useState } from "react";
 
 import Button from "@/components/button";
 
+
 export default function MyButton() {
+  const [count, setCount] = useState(0);
+
+    function handleClick(){
+      setCount(count + 1);
+    }
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-4">
-      <Button />
       <h1>React Counter Button</h1>
+      <Button onClick={handleClick} count={count}/>
+      <Button onClick={handleClick} count={count}/>
     </main>
   );
 }
--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
 
-export default function Button(){
-    const [count, setCount] = useState(0);
+interface ButtonProps {
+    count: number,
+    onClick: () => void;
+  }
 
-    function handleClick(){
-      setCount(count + 1);
-    }
-    return <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2">I have been clicked {count} times</button>;
+export default function Button({onClick, count}: ButtonProps){
+    return (
+        <button
+        onClick={onClick}
+        className="bg-blue-500 hover:bg-blue-700 rounded text-white font-bold px-4 py-2">
+            I have been clicked {count} times
+        </button>
+    );
 }
